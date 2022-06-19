891d5197b3aabf4eaa6f675b1f1fa69f7b5ba313
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2022-06-19 16:02:46 +0200

    Added on device translator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_mlkit_translation/google_mlkit_translation.dart';
 import 'package:translate_swedish/my_app.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
+  final translatorModelManager = OnDeviceTranslatorModelManager();
+  final bool isModelDownloaded = await translatorModelManager.isModelDownloaded(TranslateLanguage.swedish.bcpCode);
+  if (!isModelDownloaded) {
+    await translatorModelManager.downloadModel(TranslateLanguage.swedish.bcpCode);
+  }
   runApp(const MyApp());
 }
--- lib/services/media_service.dart
+++ lib/services/media_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:google_mlkit_translation/google_mlkit_translation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:translate_swedish/models/image_model.dart';
 
@@ -6,6 +7,7 @@ class MediaService {
   final ImagePicker _picker = ImagePicker();
   final TextRecognizer _recognizer = TextRecognizer();
   ImageModel? image;
+  final OnDeviceTranslator _onDeviceTranslator = OnDeviceTranslator(sourceLanguage: TranslateLanguage.swedish, targetLanguage: TranslateLanguage.english);
 
   Future<ImageModel?> pickFromGallery() async {
     try {
@@ -32,13 +34,17 @@ class MediaService {
   Future<List<String>> getText(String path) async {
     final inputImage = InputImage.fromFilePath(path);
     final textDetector = await _recognizer.processImage(inputImage);
-
-    List<String> recognizedList = [];
+    List<String> recognizedSwedishList = [];
+    List<String> recognizedTranslatedList = [];
     for (TextBlock block in textDetector.blocks) {
-      recognizedList.add(
+      recognizedSwedishList.add(
         block.text
       );
+      recognizedTranslatedList.add(
+        await _onDeviceTranslator.translateText(block.text)
+      );
     }
-    return recognizedList;
+    print(recognizedTranslatedList);
+    return recognizedSwedishList;
   }
 }
